bf3c752c2d0686f0c3b958b6b4258394e313b00a
FangfangChen2023 <chinnhouhou@gmail.com>
2023-01-29 23:21:26 +0000

    I worked on the progress bar. It can add steps and show the correct portion and show the remaining steps number in the middle.

--- app/src/main/java/com/example/kotlin_2/screen/HomeScreen.kt
+++ app/src/main/java/com/example/kotlin_2/screen/HomeScreen.kt
@@ -1,8 +1,6 @@
 package com.example.kotlin_2.screen
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -12,55 +10,74 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.kotlin_2.R
-import kotlin.math.round
+import com.example.kotlin_2.customComponents.CustomProgressBar
 
 @Composable
-fun HomeScreen(){
-    var progress by remember { mutableStateOf(0.1f) }
-    val animatedProgress = animateFloatAsState(
-        targetValue = progress,
-        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
-    ).value
-    Column(Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(Modifier.height(90.dp))
-        Text("Today", fontSize = 30.sp)
-        Spacer(Modifier.height(50.dp))
-        CircularProgressIndicator(
-            progress = animatedProgress,
-            color = Color.Cyan,
-            modifier = Modifier.then(Modifier.size(200.dp))
+fun HomeScreen() {
+    var steps by remember { mutableStateOf(0) }
+    var stepsInput by remember { mutableStateOf(0) }
+
+    Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(Modifier.height(60.dp))
+        Text(
+            text = "Today",
+            fontSize = MaterialTheme.typography.h4.fontSize,
+            fontWeight = FontWeight.Bold
         )
-        Spacer(Modifier.height(50.dp))
-        var text by remember { mutableStateOf(TextFieldValue("")) }
+        Spacer(Modifier.height(30.dp))
+        CustomProgressBar(
+            indicatorValue = steps
+        )
+
+        Spacer(Modifier.height(30.dp))
+
         OutlinedTextField(
             modifier = Modifier
                 .background(Color.Transparent),
-            value = text,
+
+            value = stepsInput.toString(),
             onValueChange = {
-                text = it
+                stepsInput = if (it.isNotEmpty()) {
+                    it.toInt()
+                } else {
+                    0
+                }
+
             },
             label = { Text(text = "Add Steps") },
+            placeholder = { Text(text = "0") },
             colors = TextFieldDefaults.outlinedTextFieldColors(
                 focusedBorderColor = Color.Cyan,
-                unfocusedBorderColor = Color.Cyan),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                unfocusedBorderColor = Color.Cyan
+            ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            singleLine = true
+
         )
+
         Spacer(Modifier.height(30.dp))
+
         OutlinedButton(
             modifier = Modifier
                 .background(Color.Transparent),
             shape = CircleShape,
             colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = R.color.teal_200)),
             onClick = {
-                if (progress < 1f) progress += 0.1f
+                steps += stepsInput
+                stepsInput = 0
             }
         ) {
             Text("SUBMIT", color = Color.White)
         }
     }
 }
+
